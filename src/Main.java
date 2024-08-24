1c55b1f45f2edd5e5c253d6f69af5e319a47e7ad
Admin <maayush235@gmail.com>
2024-08-24 18:57:28 +0530

    Radhey

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 class Main{
     public static void main(String args[]){
 
-//        System.out.println("Hello World");
-
-//        int arr[10] = 10;
-
+        System.out.println("Hello World");
 
     }}
 
